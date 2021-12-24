366c28adcdc0fad4db226269c6c9c3f24ce876f0
Joshua Oyewole <joshpp2013@gmail.com>
2021-12-24 03:16:16 +0100

    tailwind configuration files added

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000',
+      'red': 'red',
+      'off-red': 'rgba(226, 29, 0, 0.05)',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#evebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'primary': "#014756", 
+      'secondary': "#FFF61A",
+      'brand': "#274501",
+      'tertiary': "#064351",
+      'footerHeading': "#949580",
+      'footer' : '#010101'
+    },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'Montserrat': ['Montserrat','sans-serif'] 
+      },
+     
+  },
+  plugins: [],
+}
